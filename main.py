b6b56c1294bb591ca4565f007d79c9573cc208ad
katherine_fier <katherinefier@Katherines-MacBook-Pro.local>
2022-08-19 13:02:50 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+friends = ["marie", "mary", "jon", "dave", "ted", "mark", "larry", "helen", "sherry", "jen", "emily", "emilia", "kelly", "aaron", "erin", "liz", "kim", "anna", "jena", "carol"]
+
+dinner = friends[:4]
+print(dinner)
+
+drinks = friends[16:]
+print (drinks)
+
+hike = friends[10:14]
+print (hike)
+
+morning_walk = friends[8]
+print (morning_walk)
+            
